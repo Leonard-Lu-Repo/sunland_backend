e9fdac9f736f62a1ca9da407a06ecba6cb6ffffb
Leonard-Lu-Repo <xinglonglu565@gmail.com>
2025-02-24 21:30:44 -0500

    修改ip

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, "0,0,0,0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
