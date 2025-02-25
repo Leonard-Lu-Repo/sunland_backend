e73dfe8bfceda36f44cad8dbfffec7d69dc72b87
Leonard-Lu-Repo <xinglonglu565@gmail.com>
2025-02-24 21:20:34 -0500

    处理option请求

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // 允许所有来源
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", routes);
